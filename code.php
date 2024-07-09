6f30fef7a51139438591af8fb99012d8d5987406
Ioannis Papikas <papikas.ioan@gmail.com>
2024-07-09 14:25:50 +0300

    Add code.php for testing

---
+++ code.php
@@ -0,0 +1,18 @@
+<?php
+
+class SubClass extends BaseClass {
+	protected $private; // Allowed
+	public $private;  // Allowed
+	public $protected;  // Allowed
+
+	private $protected; // NOT ALLOWED
+}
+
+
+class BadCar {
+	public function createCar() {}
+	public function createLimousine() {}
+	public function createSUV() {}
+	public function createSuperCar() {}
+	/** and the list goes on... */
+}
